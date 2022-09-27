978b812689b012f638368fd3077a1941d30e70a8
Yu Sasaki <ysasaki.g.drive@gmail.com>
2022-09-27 21:58:08 +0900

    initial commit

---
+++ app/src/main/java/com/example/fcmtutorial1app/NotificationSettings.java
@@ -0,0 +1,7 @@
+package com.example.fcmtutorial1app;
+
+public class NotificationSettings {
+    public static String HubName = "yusasakiNewHub";
+    public static String HubListenConnectionString = "Endpoint=sb://yusasakiNewNS1.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=tvnQwg0IDwA03NIAeoBHQZaloUgTJMks8+odPKR4BZM=";
+}
+
